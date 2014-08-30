27400e0c9ec760db6aad680b406fd9c08da72252
InputUsername (Windows 8 PC) <koen.bolhuis@gmail.com>
2014-08-30 18:42:06 +0200

    More Ruby
    Finished 17, works.

--- ruby/17.rb
+++ ruby/17.rb
@@ -1,33 +1,35 @@
 # project euler 17: find the amount of letters used if the numbers 1 to 1000 were to be written out
 t = Time.new.to_f
 
-#split a number into digits
-def sp(n)
-	n.to_s.split('').map! {|n|
-		n.to_i
-	}
+#split n into digits
+def d(n)
+	n.to_s.chars.map(&:to_i)
 end
-def written_number(n)
-	if n.between?(1,9) then
-		return ["one","two","three","four","five","six","seven","eight","nine"][n-1]
-	else n.between?(10,99) then
-		if n<20 then
-			return ["ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"][n-10]
-		else
-			ret = ["twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"][sp(n)[0]-1]
-			ret += "-" + written_number(sp(n)[1]-1)
-				unless [20,30,40,50,60,70,80,90].include? n
+#return the number, written out. does not include spaces/hyphens so output can be used directly
+def written(n)
+	ret = ""
+	if (1..9).include? n #numbers 1-9
+		ret = ["one","two","three","four","five","six","seven","eight","nine"][n - 1]
+	elsif (10..19).include? n #numbers 10-19
+		ret = ["ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"][n - 10]
+	elsif (20..99).include? n #numbers 20-99
+		digits = d(n)
+		ret = ["twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"][digits[0] - 2]
+		if n%10 != 0 then ret += written(digits[1]) end
+	elsif (100..999).include? n #numbers 100-999
+		digits = d(n)
+		ret = written(digits[0]) + "hundred"
+		if n%100 != 0
+			ret += "and"
+			ret += written(digits[1,2].join.to_i)
 		end
-	else n.between?(100,999) then
-		s = sp(n)
-		ret = written_number(s[0]-1) + " hundred"
-		ret += " and " + written_number(s[1..s.length] #TODO: continue here
-		if [100,200,300,400,500,600,700,800,900].include? n then
-	else if n == 1000 then
-		return "one thousand"
+	else #number 1000
+		ret = "onethousand"
 	end
+	return ret
 end
-arr = []
-for i in (1..1000)
-
+sum = 0
+for i in 1..1000
+	sum += written(i).length
 end
+puts "result: #{sum} - found in #{((Time.new.to_f - t)*1000).round 3} ms"
