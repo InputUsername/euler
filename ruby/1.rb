8742c551157d372ba4841244b430546dafe8a672
InputUsername (Fast PC) <koen.bolhuis@gmail.com>
2014-09-07 13:28:37 +0200

    Ruby: problem 1
    Made it into a one-liner.

--- ruby/1.rb
+++ ruby/1.rb
@@ -1,7 +1,4 @@
 # project euler 1: find the sum of all multiples of 3 or 5 below 1000
 t = Time.new.to_f
-sum = 0
-for i in 0...1000 do
-	sum += i if i%3==0 || i%5==0
-end
+sum = (0..999).inject{|sum,i| i%3==0||i%5==0 ? sum+i : sum}
 puts "result: #{sum} - found in #{((Time.new.to_f - t)*1000).round 3} ms"
