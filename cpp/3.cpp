5af50ca77882b217c52a00c708db96c9befe87f4
InputUsername (Windows 8 PC) <koen.bolhuis@gmail.com>
2014-10-25 19:15:07 +0200

    Added build files to .gitignore

---
+++ cpp/3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+/* Project Euler 3
+ * Find the largest prime factor of 600,851,475,143
+ */
+int main() {
+    long long number = 600851475143;
+
+
+}
