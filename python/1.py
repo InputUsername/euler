3516f18c2ede072caf81328622a4a8b9bc63db03
InputUsername (Fast PC) <koen.bolhuis@gmail.com>
2014-11-20 17:28:43 +0100

    Python: shortened 1 and 2
    Comprehending list comprehensions, jolly good fun

--- python/1.py
+++ python/1.py
@@ -1,8 +1,3 @@
 # Project Euler 1: find the sum of all multiples of 3 and 5 below 1000
 
-sum = 0
-for i in range(0,1000):
-	if i%3 == 0 or i%5 == 0:
-		sum += i
-
-print(sum)
+print(sum(x for x in range(0,1000) if not (x % 3 and x % 5)))
--- python/2.py
+++ python/2.py
@@ -1,5 +1,6 @@
 # Project Euler 2: find the sum of all even Fibonacci numbers below 4,000,000
 
+#solution 1
 sum = 0
 a, b = 1, 1
 while a < 4000000:
@@ -8,3 +9,10 @@ while a < 4000000:
 		sum += a
 
 print(sum)
+
+#solution 2
+fib = [1,1]
+while fib[-1] < 4e6:
+	fib.append(fib[-2] + fib[-1])
+
+print(sum([x for x in fib if x % 2 == 0]))
