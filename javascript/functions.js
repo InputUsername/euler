dd68f71ec138f6c9eac8715e282338d0e8a9e930
InputUsername (Fast PC) <koen.bolhuis@gmail.com>
2014-12-12 23:02:02 +0100

    JavaScript: added a functions file
    JS doesn't support libraries AFAIK, so I thought I'd just create a file
    with useful functions

---
+++ javascript/functions.js
@@ -0,0 +1,41 @@
+var sieve = function(n) {
+	var upperBound = Math.sqrt(n);
+	var numbers = [];
+	for (var i = 0; i < n; i++) {
+		numbers.push(true);
+	}
+	
+	for (var i = 2; i <= upperBound; i++) {
+		if (numbers[i]) {
+			for (var j = i * i; j < n; j += i) {
+				numbers[j] = false;
+			}
+		}
+	}
+	
+	var output = [];
+	for (var i = 2; i < n; i++) {
+		if (numbers[i]) {
+			output.push(i);
+		}
+	}
+	
+	return output;
+}
+
+var factorize = function(n) {
+	var primes = sieve(Math.sqrt(n));
+	var factors = [];
+	var i = 0;
+	while (i < primes.length) {
+		p = primes[i];
+		if (n % p == 0) {
+			n /= p;
+			factors.push(p);
+		}
+		else {
+			i += 1;
+		}
+	}
+	return factors;
+}
