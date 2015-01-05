7fedc1b6cad71834353c83f73321d1303341332b
InputUsername <koen.bolhuis@gmail.com>
2015-01-05 21:29:49 +0100

    Updated COMPLETED list

--- completed.rb
+++ completed.rb
@@ -2,7 +2,7 @@
 
 completed_problems = {}
 
-regex = /.+\/([A-Za-z_]*[0-9]+)\..+/
+regex = /.+\/[A-Za-z_]*([0-9]+)\..+/
 
 file_whitelist = %w(.git .gitignore files libraries COMPLETED FINISHED README TODO update.rb)
 
